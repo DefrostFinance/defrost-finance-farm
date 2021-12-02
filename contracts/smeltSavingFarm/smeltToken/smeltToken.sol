d7ae19337915bb7b4c6672a20d2a3b2acc674f30
Jeffqg123 <forestjqg@126.com>
2021-12-02 16:53:11 +0800

    ..

--- contracts/smeltSavingFarm/smeltToken/smeltToken.sol
+++ contracts/smeltSavingFarm/smeltToken/smeltToken.sol
@@ -151,6 +151,12 @@ contract smeltToken is IERC20,Ownable{
     function transferFrom(address from, address to, uint256 value) public returns (bool) {
         _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
         _transfer(from, to, value);
+
+        if(enableFarm) {
+            IFARM(farm).update(from);
+            IFARM(farm).update(to);
+        }
+
         emit Approval(from, msg.sender, _allowed[from][msg.sender]);
         return true;
     }
